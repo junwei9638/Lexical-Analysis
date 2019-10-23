8b7e0cf7e954a3ac9b7de79d083eeb7fdf972361
runruncar0744 <runruncar0744@gmail.com>
2019-10-24 00:09:12 +0800

    Initial Commit

---
+++ LexicalAnalysis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LexicalAnalysis
+//
+//  Created by Junwei on 2019/10/24.
+//  Copyright © 2019 Junwei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
